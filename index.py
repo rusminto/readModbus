9e2102388bb34f92e633358967c438c98e5528e8
rusminto <rusminto900@gmail.com>
2022-12-22 14:59:24 +0700

    [MUHAM] test run, speed, off

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import crcmod.predefined
 import time
 
 ser = serial.Serial(
-    port='COM3',
+    port='COM4',
     baudrate=115200,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
@@ -34,8 +34,9 @@ def send_data(hex_command = ""):
     s_formatted = s.hex(' ').upper()
     print("RX -> ",s_formatted)
 
-def convert_to_modbus(is_read, node_id, index, sub_index, data):
+def convert_to_modbus(is_read, index, sub_index, data):
     
+    node_id = "00"
     modbus_slave_id = "05"
     modbus_function_block = "2B"
      
@@ -55,4 +56,36 @@ def convert_to_modbus(is_read, node_id, index, sub_index, data):
     send_data(command)
 
 
-convert_to_modbus(False, "01", "20 09", "00", "7F")
+# EXAMPLE
+# convert_to_modbus(False, "20 09", "00", "7F")
+
+# SET SPEED TO 50 RPM
+convert_to_modbus(False, "60 FF", "00", "32 00 00 00")
+
+# RUN
+convert_to_modbus(False, "60 40", "00", "06 00")
+convert_to_modbus(False, "60 40", "00", "0F 00")
+
+time.sleep(3)
+
+# SET SPEED TO 100 RPM
+convert_to_modbus(False, "60 FF", "00", "64 00 00 00")
+
+time.sleep(3)
+
+# SET SPEED TO 300 RPM
+convert_to_modbus(False, "60 FF", "00", "2C 01 00 00")
+
+time.sleep(3)
+
+# SET SPEED TO 100 RPM
+convert_to_modbus(False, "60 FF", "00", "64 00 00 00")
+
+time.sleep(3)
+
+# SET SPEED TO 50 RPM
+convert_to_modbus(False, "60 FF", "00", "32 00 00 00")
+
+
+#STOP
+convert_to_modbus(False, "60 40", "00", "00 00")
